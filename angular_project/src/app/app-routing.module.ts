9a89e28b579bdf62c29740f8a94da6c84689cbfd
Jambukiya Harsh Sureshbhai <harsh.jambukiya@vedikin.com>
2023-10-02 21:24:54 +0530

    Movie_review

--- angular_project/src/app/app-routing.module.ts
+++ angular_project/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',redirectTo :'login' ,pathMatch : 'full'},
+  {path : 'header' , component :HeaderComponent},
+  {path : 'home' , component :HomeComponent},
+  {path : 'login' , component :LoginComponent},
+  {path : 'movie' , component :MovieComponent},
+  {path: '**', component:LoginComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
